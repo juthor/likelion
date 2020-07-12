efd4ab0dd92bb470aad02cefb80679c33d2ad9c5
Hyunju <jemmaa1@naver.com>
2020-07-13 02:03:51 +0900

    reactjango project add

---
+++ 1. html study/123.py
@@ -0,0 +1,11 @@
+
+
+
+lion_we_know={'hyojin', 'seol', 'eunjin', 'kijung'}
+lion_meet={'hyojin', 'eunjin', 'bak'}
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hello")
+    else:
+        print("first meet")
